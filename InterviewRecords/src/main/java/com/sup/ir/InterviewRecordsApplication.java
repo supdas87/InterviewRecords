36245b97013275aace2fa1a3d9aab80ddec89b15
Supriya Das <supdas87@gmail.com>
2021-03-11 21:58:51 +0530

    First commit

---
+++ InterviewRecords/src/main/java/com/sup/ir/InterviewRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.sup.ir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewRecordsApplication.class, args);
+	}
+
+}
---
+++ InterviewRecords/src/test/java/com/sup/ir/InterviewRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sup.ir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
